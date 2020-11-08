7086f099d6ad099a6e16e7ecea8bef323b2af68c
jnoony <tarot0312@nate.com>
2020-11-09 00:46:04 +0900

    react 생성

---
+++ README.md
@@ -0,0 +1,3 @@
+## Ransun ducku
+
+### 준비중
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="rd-wrap">
+      <header></header>
+      <div>
+        준비중
+      </div>
+    </div>
+  );
+}
+
+export default App;
